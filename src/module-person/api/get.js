8d32abf18532baf7ec15d5c055300b89fc457ceb
Oleg Moiseev <guest363@gmail.com>
2021-11-27 15:06:47 +0300

    Fix: get all

--- src/module-person/api/get.js
+++ src/module-person/api/get.js
@@ -4,7 +4,7 @@ export const get = (req, res, db) => {
   const searcheResult = personUrlReqExp.exec(req.url);
   const isGetAll = searcheResult[0] === "/person";
 
-  if (!isUUIDInUrl.test(searcheResult[0])) {
+  if (!isGetAll && !isUUIDInUrl.test(searcheResult[0])) {
     return res
       .writeHead(400, { "Content-Type": "text/plain" })
       .end("Get availeble on url /person/UUID, but you send invalid UUID");
@@ -14,9 +14,7 @@ export const get = (req, res, db) => {
    * Либо возвращаем все знгачения Мапы
    * Либо берем по ID который будет в searcheResult[0]
    */
-  const responsValue = isGetAll
-    ? JSON.stringify([...db.values()])
-    : db.get(searcheResult[0]);
+  const responsValue = isGetAll ? [...db.values()] : db.get(searcheResult[0]);
 
   if (!isGetAll) {
     if (!isUUID.test(searcheResult[0])) {
